29bff0756cd9ec0a46c42c7f4e2cbb89dc2d5b4b
Ishaan Khurana <khuranaishaan5@gmail.com>
2020-09-27 23:37:52 -0400

    CORS enabled

--- src/main/java/com/cb/CBUsersService.java
+++ src/main/java/com/cb/CBUsersService.java
@@ -7,7 +7,12 @@ import io.dropwizard.Application;
 import io.dropwizard.jdbi.DBIFactory;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.skife.jdbi.v2.DBI;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class CBUsersService extends Application<DatabaseConfiguration> {
     @Override
@@ -17,6 +22,17 @@ public class CBUsersService extends Application<DatabaseConfiguration> {
 
     @Override
     public void run(DatabaseConfiguration configuration, Environment environment) throws Exception {
+        // Enable CORS headers
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
 
         final DBIFactory dbiFactory = new DBIFactory();
         final DBI dbi = dbiFactory.build(environment, configuration.getDataSourceFactory(), "mysql");
